8af3ee1330b85f9efea47b83610fc3f9c6a20409
mp3drink <52222549+mp3drink@users.noreply.github.com>
2019-06-26 08:38:00 +0700

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -1,5 +1,5 @@
 <?php
- echo "<font color ='#ffbb00'>Siapa anda?</font><br>\n";
+ echo "\e[91;1m Siapa anda?\n";
  echo "Anda telah menjalankan script saya\n";
  echo "--------------------------------------\n";
  echo "|                                    |\n";
