0ca9c08ca421e3ab8d2ce4721bcca46623ec02f3
mp3drink <52222549+mp3drink@users.noreply.github.com>
2019-06-26 08:59:09 +0700

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -1,14 +1,14 @@
 <?php
  echo "\e[91;1m Siapa anda?\n";
- sleep(1)
+ sleep(1);
  echo "Anda telah menjalankan script saya\n";
- sleep(3)
+ sleep(3);
  echo "--------------------------------------\n";
  echo "|                                    |\n";
  echo "|             ANDA SUPLUK            |\n";
  echo "|                                    |\n";
  echo "--------------------------------------\n";
- sleep(5)
+ sleep(5);
  echo "\e[32m Wkwkwk\n";
  echo "\e[0m";
  ?>
