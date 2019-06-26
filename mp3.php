719716a2f63a1ed30bb760d15a1f06dd34671a6a
mp3drink <52222549+mp3drink@users.noreply.github.com>
2019-06-26 08:19:13 +0700

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -1,6 +1,6 @@
 <?php
  echo "Siapa anda?\n";
- echo "Anda telah menjalankan script saya\n"
+ echo "Anda telah menjalankan script saya\n";
  echo "--------------------------------------\n";
  echo "|                                    |\n";
  echo "|             ANDA SUPLUK            |\n";
